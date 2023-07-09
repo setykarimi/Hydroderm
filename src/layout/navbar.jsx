75f0cf3e730eb1cfa0a9fef2f2f0049c0edbbf9e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-09 22:29:08 +0430

    Subject: layout, Description: Create new folder for layout - navbar, Date: 7/9/2023

---
+++ src/layout/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <navbar>
+            <div className="nav_logo">logo</div>
+            <div className="nav_menu">
+                <div className="nav_menu-items">menu item</div>
+            </div>
+        </navbar>
+    );
+}
+
+export default Navbar;
