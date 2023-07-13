c263fc9c3e7c7bdc3b9b5b58ae4030ded1cd1eae
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-13 09:51:00 +0430

    Subject: Banner, Description: Add bbaner, Date: 7/13/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Banner from './components/banner/banner'
 import Navbar from './layout/navbar'
 
 function App() {
   return (
     <div className='container'>
       <Navbar />
+      <Banner />
      </div>
   )
 }
