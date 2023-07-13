f36de671639f5305725688838502a9aeca47df60
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-13 09:50:22 +0430

    Subject: components, Description: create new folder for components- create banner component, Date: 7/13/2023

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,24 @@
+import union from '../../assets/images/icons/union.svg'
+import magic from '../../assets/images/icons/magic.svg'
+const Banner = () => {
+    return ( 
+        <div className="banner">
+            <div>
+                <button className="banner-inroducing">
+                    <img src={union} alt="icon"/>
+                    Introducing Image Feature
+                </button>
+                <h1 className='banner-title'>Image with Serendipity</h1>
+                <p className='banner-desc'>
+                Discover a vibrant community of creators on Serendipity! Connect with thousands of like-minded individuals on Discord or the web and unleash your imagination through collaborative storytelling. From vivid worlds to unforgettable characters, bring your short text descriptions to life in new and exciting ways. Join Serendipity today and embark on a journey of endless creativity!
+                </p>
+                <button className='banner-generate'>
+                    <img src={magic} alt="icon" />
+                    Generate Images</button>
+            </div>
+            <div></div>
+        </div>
+     );
+}
+ 
+export default Banner;
