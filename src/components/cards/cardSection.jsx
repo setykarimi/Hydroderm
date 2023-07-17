1758a0d2b6108926cfb86f8f7e3934b13a369d06
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-17 21:20:56 +0430

    Subject: card-section, Description: Create new component for card-section, Date: 7/17/2023

---
+++ src/components/cards/cardSection.jsx
@@ -0,0 +1,18 @@
+const CardSection = () => {
+    return ( 
+        <div className="card-section">
+            <img src="" alt=""/>
+            <div className="card-section_details">
+                <h3 className="card-section_details-title"></h3>
+                <div className="card-section_details_desc">
+                    <span className="card-section_details_desc-title"></span>
+                    <p className="card-section_details_desc-detail"></p>
+                </div>
+
+                <button className="card-section_button"></button>
+            </div>
+        </div>
+     );
+}
+ 
+export default CardSection;
