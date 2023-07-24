b1447ef05c046f846eb3af44d60b3ed7d4cc9fab
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-24 21:16:19 +0430

    Subject: Old Version, Description: Remove old version landing, Date: 7/24/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css'
-import Banner from './components/banner/banner'
-import CardSection from './components/cards/cardSection'
-import Navbar from './layout/navbar'
 
 function App() {
   return (
-    <div className='container'>
-      <Navbar />
-      <Banner />
-      <CardSection />
+    <div className='container mx-auto'>
+     <h1>Hello</h1>
      </div>
   )
 }
