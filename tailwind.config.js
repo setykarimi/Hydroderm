3506fbe820794cc031507fa4765350df08008287
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-26 20:32:34 +0430

    Subject: BoxShadow, Description: Add md BoxShadow to config, Date: 7/26/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       blue:  "#3156A4",
       white: "#fff",
       gray: "#3C3C3C"
+    },
+    boxShadow: {
+      md: '0px 2px 6px 0px rgba(49, 86, 164, 0.50)'
     }
   },
   plugins: [],
