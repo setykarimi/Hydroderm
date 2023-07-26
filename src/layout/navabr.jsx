891e81106c5429012c417e80fd1b8db4604cc86a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-26 20:32:15 +0430

    Subject: Navbar, Description: Add BoxShadow, Date: 7/26/2023

--- src/layout/navabr.jsx
+++ src/layout/navabr.jsx
@@ -1,7 +1,7 @@
-import logo from "Images/logo.svg"
-import search from "Icons/search.svg"
 import headphone from "Icons/headphone.svg"
 import menu from "Icons/menu.svg"
+import search from "Icons/search.svg"
+import logo from "Images/logo.svg"
 
 const Navbar = () => {
     const navItem = [
@@ -9,19 +9,20 @@ const Navbar = () => {
     ]
     return (
         <nav className="md:flex grid grid-cols-3 justify-between items-center py-4">
-        <button className="md:hidden block py-2 md:px-4 px-2 ">
-                    <img src={menu} alt="search" />
-                </button>
-            <div className="flex items-center md:justify-start justify-center gap-12"><img src={logo} alt="logo" />
+            <button className="md:hidden block py-2 md:px-4 px-2">
+                <img src={menu} alt="search" />
+            </button>
+            <div className="flex items-center md:justify-start justify-center gap-12">
+                <img src={logo} alt="logo" />
                 <div className="md:flex hidden gap-8">
                     {navItem.map((name) => <span className="text-sm text-gray-700">{name}</span>)}
                 </div>
             </div>
             <div className="flex gap-4 justify-end">
-                <button className=" bg-blue py-2 md:px-4 px-2 md:w-auto md:h-auto w-8 h-8 rounded-lg">
+                <button className=" bg-blue py-2 shadow-md md:px-4 px-2 md:w-auto md:h-auto w-8 h-8 rounded-lg">
                     <img src={search} alt="search" />
                 </button>
-                <button className=" bg-blue py-2 md:px-4 px-2 md:w-auto md:h-auto w-8 h-8 rounded-lg flex gap-2 items-center">
+                <button className=" bg-blue py-2 shadow-md md:px-4 px-2 md:w-auto md:h-auto w-8 h-8 rounded-lg flex gap-2 items-center">
                     <span className="text-sm text-white font-bold md:block hidden">مشاوره تخصصی</span>
                     <img src={headphone} alt="search" />
                 </button>
