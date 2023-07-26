04f06502fbb0aaa5fd04e469c730ae2a4ad3b573
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 00:38:52 +0430

    Subject: Home page, Description: Add Brand, Date: 7/26/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Brands from './components/brands/brands'
+import HeroSection from './components/hero/heroSection'
 import Navbar from './layout/navabr'
 
 function App() {
   return (
     <div className='lg:container lg:px-0 px-4 mx-auto'>
       <Navbar />
+      <HeroSection />
+      <Brands />
     </div>
   )
 }
