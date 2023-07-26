076bc772756601feeb37686a3e8f165bed0fbb6e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 00:38:37 +0430

    Subject: config, Description: Add new boxShadow, Date: 7/26/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ export default {
       transparent: "transparent"
     },
     boxShadow: {
+      xs: '0px 1px 6px 0px #D3DCEE',
+      sm: '0px 2px 4px 0px #BDCBE6',
       md: '0px 2px 6px 0px rgba(49, 86, 164, 0.50)'
     }
   },
