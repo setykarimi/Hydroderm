ec652c01a2f6dd6b36a79e226e0ea17c32b82ff5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-26 22:15:59 +0430

    Subject: Title, Description: Create new folder for titles, Date: 7/26/2023

---
+++ src/components/title/titleLine.jsx
@@ -0,0 +1,17 @@
+import arrow from "Icons/arrowLeft.svg"
+
+const TitleLine = ({name}) => {
+    return ( 
+        <div className="flex gap-6 items-center justify-between mt-16">
+            <span className="font-bold md:text-2xl text-xl text-blue">{name}</span>
+            <span className="block bg-gradient-to-r from-transparent from-0% via-[#D3DCEE] via-50% to-transparent to-100% h-[2px] w-[100%]"></span>
+            <button className="text-blue whitespace-nowrap flex items-center gap-2 text-sm">مشاهده بیشتر
+            <img src={arrow} alt="arrow" />
+            </button>
+        </div>
+     );
+}
+
+
+ 
+export default TitleLine;
