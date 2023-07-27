66cc00d95dd479a1b8db51c2c2a420b608e3c667
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 11:57:47 +0430

    Subject: Homepage, Description: Add new components- edit style, Date: 7/27/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
+import Blog from './components/blog/blog'
 import Brands from './components/brands/brands'
 import HeroSection from './components/hero/heroSection'
+import OnlineShop from './components/onlineShop/onlineShop'
 import Popular from './components/popular/popular'
 import SpecialProduct from './components/specialProduct/specialProduct'
 import Navbar from './layout/navabr'
 
 function App() {
   return (
-    <div className='lg:container lg:px-0 px-4 mx-auto'>
+    <div className='lg:container px-4 mx-auto'>
       <Navbar />
       <HeroSection />
       <Brands />
       <Popular />
       <SpecialProduct />
+      <OnlineShop />
+      <Blog />
     </div>
   )
 }
