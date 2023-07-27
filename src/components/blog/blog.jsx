0351175f51696da8cfcc9c8eed6adf7ccab5dc2a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 11:58:06 +0430

    Subject: Blog, Description: Crerate new folder for blog, Date: 7/27/2023

---
+++ src/components/blog/blog.jsx
@@ -0,0 +1,11 @@
+import TitleLine from "../title/titleLine";
+
+const Blog = () => {
+    return ( 
+        <div>
+            <TitleLine name="آخرین مطالب بلاگ" />
+        </div>
+     );
+}
+ 
+export default Blog;
