d5ee1000b018edf6416c38da6dcaec1deeff2b7f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 13:09:32 +0430

    Subject: Blog, Description: Edit style for responsive view, Date: 7/26/2023

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -39,14 +39,14 @@ const Blog = () => {
 
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
                 {blogs.map(({ logo, bg, date, title }) => (
-                    <div className="rounded-xl shadow-sm" style={{ background: `url(${bg}) no-repeat`, backgroundSize: 'cover' }}>
+                    <div className="rounded-xl flex flex-col gap-12 shadow-sm" style={{ background: `url(${bg}) no-repeat`, backgroundSize: 'cover' }}>
                         <div className="bg-white p-2 w-16 h-16 rounded-tr-xl rounded-bl-xl flex justify-center items-center">
                             <img src={logo} alt="logo" />
                         </div>
-                        <div className="flex justify-between p-4 mt-12 backdrop-blur-md	items-center rounded-b-xl" style={{background: 'rgba(49, 86, 164, 0.40)'}}>
+                        <div className="flex justify-between p-4 mt-auto backdrop-blur-md items-center rounded-b-xl" style={{background: 'rgba(49, 86, 164, 0.40)'}}>
                             <div>
                                 <span className="block text-white text-xs">{date}</span>
-                                <span className="block text-white mt-2">{title}</span>
+                                <span className="block text-white mt-2 xl:text-base text-sm">{title}</span>
                             </div>
                             <button className="bg-blue text-white py-2 px-4 rounded-lg font-bold text-sm h-fit">مطالعه</button>
                         </div>
