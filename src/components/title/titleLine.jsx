4c7c082a1a1be2f7a4a857798b0e60f3785a3ea0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 11:57:22 +0430

    Subject: TitleLine, Description: Edit for responsive view, Date: 7/27/2023

--- src/components/title/titleLine.jsx
+++ src/components/title/titleLine.jsx
@@ -1,17 +1,17 @@
 import arrow from "Icons/arrowLeft.svg"
 
-const TitleLine = ({name}) => {
-    return ( 
+const TitleLine = ({ name }) => {
+    return (
         <div className="flex gap-6 items-center justify-between mt-16 mb-4">
-            <h2 className="font-bold md:text-2xl text-xl text-blue">{name}</h2>
+            <h2 className="font-bold md:text-2xl text-xl text-blue whitespace-nowrap">{name}</h2>
             <span className="block bg-gradient-to-r from-transparent from-0% via-[#D3DCEE] via-50% to-transparent to-100% h-[2px] w-[100%]"></span>
             <button className="text-blue whitespace-nowrap flex items-center gap-2 text-sm">مشاهده بیشتر
-            <img src={arrow} alt="arrow" />
+                <img src={arrow} alt="arrow" />
             </button>
         </div>
-     );
+    );
 }
 
 
- 
+
 export default TitleLine;
