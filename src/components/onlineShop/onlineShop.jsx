dbd44f966ef88163a359ebed6f5e57299c5a9a7e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-27 11:56:57 +0430

    Subject: onlineShop, Description: Create new folder for onlineShops, Date: 7/27/2023

---
+++ src/components/onlineShop/onlineShop.jsx
@@ -0,0 +1,56 @@
+import TitleLine from "../title/titleLine"
+import bgWaves from "Images/onlineShop/bg-waves.svg"
+
+import mootanroLogo from "Images/onlineShop/mootanro.svg"
+import mootanroWeb from "Images/onlineShop/mootanro.png"
+
+import rojaLogo from "Images/onlineShop/roja.svg"
+import rojaWeb from "Images/onlineShop/roja.png"
+
+import shavajLogo from "Images/onlineShop/shavaj.svg"
+import shavajWeb from "Images/onlineShop/shavaj.png"
+
+import khanoomiLogo from "Images/onlineShop/khanoomi.svg"
+import khanoomiWeb from "Images/onlineShop/khanoomi.png"
+
+
+const OnlineShop = () => {
+    const onlineShops = [
+        {
+            logo: mootanroLogo,
+            web: mootanroWeb
+        },
+        {
+            logo: rojaLogo,
+            web: rojaWeb
+        },
+        {
+            logo: shavajLogo,
+            web: shavajWeb
+        },
+        {
+            logo: khanoomiLogo,
+            web: khanoomiWeb
+        }
+    ]
+    return (
+        <div>
+            <TitleLine name="فروشگاه‌های همکار" />
+
+            <div className="grid md:grid-cols-4 grid-cols-2  gap-4">
+                {onlineShops.map(({ logo, web }) => (
+                    <div className="px-4 flex lg:flex-row flex-col lg:justify-between justify-center lg:items-start items-center rounded-xl shadow-sm" style={{ background: `url(${bgWaves}) no-repeat`, backgroundSize: 'cover' }}>
+                        <div className="bg-white rounded-lg w-20 p-4 shadow-xs h-fit mt-4">
+                            <img src={logo} alt="logo" />
+                        </div>
+                        <div className="mt-4">
+                            <img src={web} alt="web" />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default OnlineShop;
