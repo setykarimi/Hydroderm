ca1f0d256795dbf51bda48b59c8686d8628d511f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-28 17:57:27 +0430

    Subject: Style, Description: Edit style for reponsive view, Date: 7/28/2023

--- src/components/hero/heroSection.jsx
+++ src/components/hero/heroSection.jsx
@@ -1,10 +1,9 @@
-import hero1 from "Images/hero/hero-img-01.png"
-import hero2 from "Images/hero/hero-img-02.png"
-import hero3 from "Images/hero/hero-img-03.png"
-import hero4 from "Images/hero/hero-img-04.png"
-import hero5 from "Images/hero/hero-img-05.png"
-import hero6 from "Images/hero/hero-img-06.png"
-import hero7 from "Images/hero/hero-img-07.png"
+
+import hero from "Images/hero/hero-img.png"
+import heroBlur from "Images/hero/hero-img-blur.png"
+
+import heroMobile from "Images/hero/hero-img-mobile.png"
+import heroBlurMobile from "Images/hero/hero-img-blur-mobile.png"
 
 const HeroSection = () => {
     return (
@@ -13,25 +12,16 @@ const HeroSection = () => {
                 <h1 className="font-bold text-4xl text-center mb-6"><span className="text-blue">هیدورودرم،</span> راز جوانی و شادابی پوست</h1>
                 <button className="border border-blue text-blue py-2 px-4 rounded-lg text-xs mx-auto font-bold">مشاهده همه محصولات</button>
             </div>
-            <div className="flex md:gap-4 gap-2 justify-center items-center">
-                <div className="lg:block hidden">
-                    <img src={hero1} alt="image" />
+                <div className="relative w-ful flex justify-center items-center">
+                    <picture className="relative z-10 w-full">
+                        <source srcset={hero} media="(min-width: 600px)" />
+                        <img src={heroMobile} alt="hero" className="object-cover w-full"/>
+                    </picture>
+                    <picture className="absolute -top-10 left-0">
+                        <source srcset={heroBlur} media="(min-width: 600px)" />
+                        <img src="" />
+                    </picture>
                 </div>
-                <div className="flex flex-col md:gap-4 gap-2">
-                    <img src={hero2} alt="image" />
-                    <img src={hero3} alt="image" />
-                </div>
-                <div>
-                    <img src={hero4} alt="image" />
-                </div>
-                <div className="flex flex-col md:gap-4 gap-2">
-                    <img src={hero5} alt="image" />
-                    <img src={hero6} alt="image" />
-                </div>
-                <div className="lg:block hidden">
-                    <img src={hero7} alt="image" />
-                </div>
-            </div>
         </div>
     );
 }
